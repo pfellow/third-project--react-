3ce270e94c63c5ae3f3830659f5eb4ad8c5c4b99
pfellow <pfellow@gmail.com>
2023-04-11 14:08:28 +1200

    changed user list to ul/li

--- src/Components/AddUserForm/UserCard.js
+++ src/Components/AddUserForm/UserCard.js
@@ -2,9 +2,9 @@ import styles from "./UserCard.module.css";
 
 const UserCard = (props) => {
   return (
-    <div
+    <li
       className={styles["user-card"]}
-    >{`${props.user.username} (${props.user.age} year(s) old)`}</div>
+    >{`${props.user.username} (${props.user.age} year(s) old)`}</li>
   );
 };
 
--- src/Components/AddUserForm/UserList.js
+++ src/Components/AddUserForm/UserList.js
@@ -5,11 +5,11 @@ import styles from "./UserList.module.css";
 const UserList = (props) => {
   return (
     <Card>
-      <div className={styles["user-list"]}>
+      <ul className={styles["user-list"]}>
         {props.users.map((user) => (
           <UserCard key={user.id} user={user}></UserCard>
         ))}
-      </div>
+      </ul>
     </Card>
   );
 };
